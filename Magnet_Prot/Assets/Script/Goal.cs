915b51832ee0d48ac00bb92633cb4d3fd31c9a0a
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2022-04-27 22:37:13 +0900

    update

---
+++ Magnet_Prot/Assets/Script/Goal.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+//Å¶íçà”Å¶Å@âºé¿ëï
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField]
+    GameObject clearUI;
+
+    private bool timeStop = false;
+
+    private void Update()
+    {
+        if (timeStop)
+        {
+            if (Input.anyKeyDown)
+            {
+                timeStop = false;
+                Time.timeScale = 1;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            clearUI.SetActive(true);
+            Time.timeScale = 0;
+            timeStop = true;
+        }
+    }
+}
