d0e7d0ed9959e1606d038a1ba7a6b20009601d87
LAPTOP-J3K3FLA1\ARAKA <ARAKAWAYUU20010329@outlook.jp>
2022-05-10 14:31:17 +0900

    update

--- Magnet_Prot/Assets/Script/SavePointManager.cs
+++ Magnet_Prot/Assets/Script/SavePointManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class SavePointManager : MonoBehaviour
 {
-    [Header("プレイヤーの初期座標入力")]
-    public Vector2 SavePointPos;// プレイヤーが復帰するセーブポイント座標
+    private bool SaveJudge = false; // セーブ地点通ったか通ってないか判定
 
-    public bool SaveJudge = false;
+    [Header("プレイヤーの初期座標入力")]
+    public Vector2 SavePointPos;    // プレイヤーが復帰するセーブポイント座標
+
+    public bool GetSaveJadge() { return SaveJudge; }            // 判定をゲットする
+    public void SetSaveJadge(bool jadge) { SaveJudge = jadge; } // 判定をセットする
 
     public float GetSavePointPosX() { return SavePointPos.x; }
     public float GetSavePointPosY() { return SavePointPos.y; }
