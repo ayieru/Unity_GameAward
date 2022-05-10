34f0cbfb6100bd2a2eabb39a3331a307a4d0800a
yuゆ様 <79795916+ayieru@users.noreply.github.com>
2022-05-10 11:28:51 +0900

    update

--- Magnet_Prot/Assets/Script/SceneController.cs
+++ Magnet_Prot/Assets/Script/SceneController.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         FadeManager.FadeIn();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+#else
+    Application.Quit();//ゲームプレイ終了
+#endif
+        }
     }
 }
