4245a697acc0d6f03c94a72fa8de5bf83248ea96
NagayaMiyu <71922638+wonkle07@users.noreply.github.com>
2022-05-19 14:43:16 +0900

    update

---
+++ Magnet_Prot/Assets/Script/StageSelect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageSelect : MonoBehaviour
+{
+    [Header("ステージID(text)")]
+    public Text StageIDText;
+
+    //[Header("選択中のステージID")]
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
