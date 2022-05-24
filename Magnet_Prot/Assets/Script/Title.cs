04442805a6924c6ef51de98b6643e1190efc34ef
NagayaMiyu <71922638+wonkle07@users.noreply.github.com>
2022-05-24 09:22:14 +0900

    update

---
+++ Magnet_Prot/Assets/Script/Title.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    [Header("Start(text)")]
+    public Text StartText;
+
+    [Header("Exit(text)")]
+    public Text ExitText;
+
+    void Start()
+    {
+        FadeManager.FadeIn();
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+
+    }
+}
