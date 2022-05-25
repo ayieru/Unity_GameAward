7d8fb655e16d4bda39758a69e57dfc61671e8a03
NagayaMiyu <71922638+wonkle07@users.noreply.github.com>
2022-05-25 21:53:03 +0900

    update

--- Magnet_Prot/Assets/Script/Result.cs
+++ Magnet_Prot/Assets/Script/Result.cs
@@ -6,18 +6,28 @@ using UnityEngine.UI;
 public class Result : MonoBehaviour
 {
     private GameObject NextStageButton;
+    private GameObject SelectButton;
     private GameObject StageText;
 
     void Start()
     {
         FadeManager.FadeIn();
         NextStageButton = transform.Find("NextStage").gameObject;
+        SelectButton = transform.Find("Select").gameObject;
         StageText = transform.Find("StageIDText").gameObject;
 
         //最終ステージの場合
         if (Goal.IsLastStage == true)
         {
             NextStageButton.gameObject.SetActive(false);
+            Button button = SelectButton.GetComponent<Button>();
+            button.Select();
+
+        }
+        else
+        {
+            Button button = NextStageButton.GetComponent<Button>();
+            button.Select();
         }
 
         //ステージ名を表示
