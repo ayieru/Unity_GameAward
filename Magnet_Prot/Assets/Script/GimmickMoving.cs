953c798d5feb3e33e39099e8c9d0fcaf490ef272
LAPTOP-J3K3FLA1\ARAKA <ARAKAWAYUU20010329@outlook.jp>
2022-06-14 23:36:09 +0900

    移動スクリプト作成

---
+++ Magnet_Prot/Assets/Script/GimmickMoving.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickMoving : MonoBehaviour
+{
+
+    [Header("移動箇所設定")]
+    public GameObject[] MovePoint;
+
+    [Header("移動速度")]
+    public float Speed = 1.0f;
+
+
+    private Rigidbody2D rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
