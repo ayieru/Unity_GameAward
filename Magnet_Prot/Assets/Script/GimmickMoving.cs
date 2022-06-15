db613cafe029cc8c63d78a567e4b4e8f8a13a0f8
LAPTOP-J3K3FLA1\ARAKA <ARAKAWAYUU20010329@outlook.jp>
2022-06-15 17:01:42 +0900

    完成した（動作確認未）

--- Magnet_Prot/Assets/Script/GimmickMoving.cs
+++ Magnet_Prot/Assets/Script/GimmickMoving.cs
@@ -13,17 +13,73 @@ public class GimmickMoving : MonoBehaviour
 
 
     private Rigidbody2D rb;
-
+    private int NowMovePoint = 0;   // 現在の要素数
+    private bool NowReturn = false; // 戻るか戻らないか
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        // MovePointが存在する場合、かつMovePointの要素数がある場合、かつrbがあるなら
+        if (MovePoint != null && MovePoint.Length > 0 && rb != null)
+        {
+            // 初期座標設定
+            rb.position = MovePoint[0].transform.position;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (MovePoint == null || MovePoint.Length <= 0 || rb == null) return;
+
+        // 通常
+        if (!NowReturn)
+        {
+            int nextPoint = NowMovePoint + 1;
+
+            // 目標ポイントとの誤差がわずかになるまで移動
+            if (Vector2.Distance(transform.position, MovePoint[nextPoint].transform.position) > 0.1f)
+            {
+                // 現在地から次のポイントへのベクトルを作成
+                Vector2 toVector = Vector2.MoveTowards(transform.position, MovePoint[nextPoint].transform.position, Speed * Time.deltaTime);
+
+                // 次のポイントへ移動
+                rb.MovePosition(toVector);
+            }
+            // 次のポイントを１つ進める
+            else
+            {
+                rb.MovePosition(MovePoint[nextPoint].transform.position);
+                ++NowMovePoint;
+
+                // 現在地が配列の最後だった場合
+                if (NowMovePoint + 1 >= MovePoint.Length) NowReturn = true;
+            }
+        }
+        // 折返し
+        else
+        {
+            int nextPoint = NowMovePoint - 1;
+
+            // 目標ポイントとの誤差がわずかになるまで移動
+            if (Vector2.Distance(transform.position, MovePoint[nextPoint].transform.position) > 0.1f)
+            {
+                // 現在地から次のポイントへのベクトルを作成
+                Vector2 toVector = Vector2.MoveTowards(transform.position, MovePoint[nextPoint].transform.position, Speed * Time.deltaTime);
+
+                // 次のポイントへ移動
+                rb.MovePosition(toVector);
+            }
+            // 次のポイントを１つ戻す
+            else
+            {
+                rb.MovePosition(MovePoint[nextPoint].transform.position);
+                --NowMovePoint;
+
+                // 現在地が配列の最初だった場合
+                if (NowMovePoint <= 0) NowReturn = false;
+            }
+        }
     }
 }
