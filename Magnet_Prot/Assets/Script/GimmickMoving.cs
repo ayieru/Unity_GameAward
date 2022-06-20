6c7bcfdc285814c1199df87c1bb616c8f541a684
LAPTOP-J3K3FLA1\ARAKA <ARAKAWAYUU20010329@outlook.jp>
2022-06-20 18:44:15 +0900

    とりあえず動かすのは完成(バグは未解決)

--- Magnet_Prot/Assets/Script/GimmickMoving.cs
+++ Magnet_Prot/Assets/Script/GimmickMoving.cs
@@ -6,11 +6,13 @@ public class GimmickMoving : MonoBehaviour
 {
 
     [Header("移動箇所設定")]
-    public Vector2[] MovePoint;
+    public GameObject[] MovePoint;// 次に移動する場所
 
     [Header("移動速度")]
     public float Speed = 1.0f;
 
+    [Header("最後に到達したら最初に戻すか")]
+    public bool FirstBack = false;
 
     private Rigidbody2D rb;
     private int NowMovePoint = 0;   // 現在の要素数
@@ -24,63 +26,84 @@ public class GimmickMoving : MonoBehaviour
         if (MovePoint != null && MovePoint.Length > 0 && rb != null)
         {
             // 初期座標設定
-            rb.position = MovePoint[0];
+            rb.position = MovePoint[0].transform.position;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MovePoint == null || MovePoint.Length <= 0 || rb == null) return;
+        if (MovePoint.Length <= 1 || rb == null) return;
 
-        Vector2 MovePos = this.transform.position;
-
-        // 通常
-        if (!NowReturn)
+        // 最初に戻るか
+        if(FirstBack)
         {
             int nextPoint = NowMovePoint + 1;
 
             // 目標ポイントとの誤差がわずかになるまで移動
-            if (Vector2.Distance(MovePos, MovePoint[nextPoint]) > 0.1f)
+            if (Vector2.Distance(this.transform.position, MovePoint[nextPoint].transform.position) > 0.1f)
             {
                 // 現在地から次のポイントへのベクトルを作成
-                Vector2 toVector = Vector2.MoveTowards(MovePos, MovePoint[nextPoint], Speed * Time.deltaTime);
+                Vector2 toVector = Vector2.MoveTowards(this.transform.position, MovePoint[nextPoint].transform.position, Speed * Time.deltaTime);
 
                 // 次のポイントへ移動
                 rb.MovePosition(toVector);
             }
-            // 次のポイントを１つ進める
-            else
+            else// 次のポイントを１つ進める
             {
-                rb.MovePosition(MovePoint[nextPoint]);
-                ++NowMovePoint;
+                rb.MovePosition(MovePoint[nextPoint].transform.position);
+                ++NowMovePoint;// 要素数のカウントを増やす
 
                 // 現在地が配列の最後だった場合
-                if (NowMovePoint + 1 >= MovePoint.Length) NowReturn = true;
+                if (NowMovePoint + 1 >= MovePoint.Length) NowMovePoint = -1;
             }
         }
-        // 折返し
-        else
+        else// 順番通り戻るか
         {
-            int nextPoint = NowMovePoint - 1;
-
-            // 目標ポイントとの誤差がわずかになるまで移動
-            if (Vector2.Distance(MovePos, MovePoint[nextPoint]) > 0.1f)
+            // 通常
+            if (!NowReturn)
             {
-                // 現在地から次のポイントへのベクトルを作成
-                Vector2 toVector = Vector2.MoveTowards(MovePos, MovePoint[nextPoint], Speed * Time.deltaTime);
+                int nextPoint = NowMovePoint + 1;
 
-                // 次のポイントへ移動
-                rb.MovePosition(toVector);
+                // 目標ポイントとの誤差がわずかになるまで移動
+                if (Vector2.Distance(this.transform.position, MovePoint[nextPoint].transform.position) > 0.1f)
+                {
+                    // 現在地から次のポイントへのベクトルを作成
+                    Vector2 toVector = Vector2.MoveTowards(this.transform.position, MovePoint[nextPoint].transform.position, Speed * Time.deltaTime);
+
+                    // 次のポイントへ移動
+                    rb.MovePosition(toVector);
+                }
+                else// 次のポイントを１つ進める
+                {
+                    rb.MovePosition(MovePoint[nextPoint].transform.position);
+                    ++NowMovePoint;// 要素数のカウントを増やす
+
+                    // 現在地が配列の最後だった場合
+                    if (NowMovePoint + 1 >= MovePoint.Length) NowReturn = true;
+                }
             }
-            // 次のポイントを１つ戻す
-            else
+            else// 折返し
             {
-                rb.MovePosition(MovePoint[nextPoint]);
-                --NowMovePoint;
+                int nextPoint = NowMovePoint - 1;
 
-                // 現在地が配列の最初だった場合
-                if (NowMovePoint <= 0) NowReturn = false;
+                // 目標ポイントとの誤差がわずかになるまで移動
+                if (Vector2.Distance(this.transform.position, MovePoint[nextPoint].transform.position) > 0.1f)
+                {
+                    // 現在地から次のポイントへのベクトルを作成
+                    Vector2 toVector = Vector2.MoveTowards(this.transform.position, MovePoint[nextPoint].transform.position, Speed * Time.deltaTime);
+
+                    // 次のポイントへ移動
+                    rb.MovePosition(toVector);
+                }
+                else// 次のポイントを１つ戻す
+                {
+                    rb.MovePosition(MovePoint[nextPoint].transform.position);
+                    --NowMovePoint;// 要素数のカウントを減らす
+
+                    // 現在地が配列の最初だった場合
+                    if (NowMovePoint <= 0) NowReturn = false;
+                }
             }
         }
     }
